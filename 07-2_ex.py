09ad44f9dd0a7f58e2093a5fb20c849d400fab0f
Baek Jimin <s2104@e-mirim.hs.kr>
2022-02-28 19:27:35 +0900

    flask 모듈

--- 07-2_ex.py
+++ 07-2_ex.py
@@ -1,10 +1,24 @@
-from urllib import request
-from bs4 import BeautifulSoup
+#from urllib import request
+#from bs4 import BeautifulSoup
+#
+#content = request.urlopen("http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=109")
+#soup = BeautifulSoup(content, 'html.parser')
+#
+#for data in soup.select("data"):
+#    print("시간: ", data.select_one("tmef").string)
+#    print("날짜: ", data.select_one("wf").string)
+#    print("-" * 20)
 
-content = request.urlopen("http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=109")
-soup = BeautifulSoup(content, 'html.parser')
+# save this as app.py
+from flask import Flask
 
-for data in soup.select("data"):
-    print("시간: ", data.select_one("tmef").string)
-    print("날짜: ", data.select_one("wf").string)
-    print("-" * 20)
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+"""
+$ set FLASK_APP = 07-2_ex.py
+$ flask run
+"""
